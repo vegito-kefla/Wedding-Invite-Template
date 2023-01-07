e20e88e11374574d9b9ef9f62b258f9eb738caa6
Saravana-Krishnan <Saravana.krishnan@infovision.com>
2023-01-07 16:37:11 +0530

    current commit

--- src/components/Pages/Page3.tsx
+++ src/components/Pages/Page3.tsx
@@ -27,7 +27,7 @@ export const Page3: React.FC = () => {
         </Grid>
 
         <Grid >
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.3590207053635!2d76.81432579999999!3d8.7522503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05e8216d1d96ad%3A0xefbffee302245ad6!2sKallambalam%20-%20Nagaroor%20Rd%2C%20Kallambalam%2C%20Kerala%20695605!5e0!3m2!1sen!2sin!4v1673078126770!5m2!1sen!2sin" loading="lazy" width="auto" height="450" />
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.3590207053635!2d76.81432579999999!3d8.7522503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05e8216d1d96ad%3A0xefbffee302245ad6!2sKallambalam%20-%20Nagaroor%20Rd%2C%20Kallambalam%2C%20Kerala%20695605!5e0!3m2!1sen!2sin!4v1673078126770!5m2!1sen!2sin" title="myframe" loading="lazy" width="auto" height="450" />
         </Grid>
       </Page2Container>
 
