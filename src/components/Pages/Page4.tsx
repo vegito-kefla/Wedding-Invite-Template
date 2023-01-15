24ea2ea989b9a3c737256ed3cbed147c31d7f58d
Saravana-Krishnan <Saravana.krishnan@infovision.com>
2023-01-15 23:47:08 +0530

    coe

--- src/components/Pages/Page4.tsx
+++ src/components/Pages/Page4.tsx
@@ -6,23 +6,11 @@ export const Page4: React.FC = () => {
 
     return (
         <Page3Wrapper>
-            {/* <Page2Container>
-                <h2 className="invite">
-                    Wedding Location
-                </h2>
-
-
+            <Page2Container style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "40%", marginTop: 400 }}>
                 <Grid >
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3524.142558374678!2d75.26603899999999!3d27.95892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0!2zMjfCsDU3JzMyLjEiTiA3NcKwMTUnNTcuNyJF!5e0!3m2!1sen!2sin!4v1673321827595!5m2!1sen!2sin" title="myframe" loading="lazy" width="auto" height="250" />
                 </Grid>
-                <h2 className="reception">
-                    Address :
-                    <h2 className="invite">
-                        Houseno:72,Ajeetpura,<br />Jhunjhunu(dist),Rajasthan
-                    </h2>
-                </h2>
-
-            </Page2Container> */}
+            </Page2Container>
         </Page3Wrapper >
     );
 };
