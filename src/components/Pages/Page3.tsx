8f29ebee96470db6bd3902c8c5dfa6d9bcf4f1ba
Saravana-Krishnan <Saravana.krishnan@infovision.com>
2023-01-19 23:04:33 +0530

    coe

--- src/components/Pages/Page3.tsx
+++ src/components/Pages/Page3.tsx
@@ -9,7 +9,7 @@ export const Page3: React.FC = () => {
       <Page2Container style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "40%", marginTop: 400 }} >
 
         <Grid >
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3524.142558374678!2d75.26603899999999!3d27.95892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0!2zMjfCsDU3JzMyLjEiTiA3NcKwMTUnNTcuNyJF!5e0!3m2!1sen!2sin!4v1673321827595!5m2!1sen!2sin" title="myframe" loading="lazy" width="auto" height="250" />
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3524.1104977375485!2d75.265683!3d27.959902000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0!2zMjfCsDU3JzM1LjciTiA3NcKwMTUnNTYuNSJF!5e0!3m2!1sen!2sin!4v1674149620210!5m2!1sen!2sin" width="auto" height="250" loading="lazy" ></iframe>
         </Grid>
 
       </Page2Container>
--- src/components/Pages/Page4.tsx
+++ src/components/Pages/Page4.tsx
@@ -8,7 +8,7 @@ export const Page4: React.FC = () => {
         <Page3Wrapper>
             <Page2Container style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "40%", marginTop: 400 }}>
                 <Grid  >
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.3590207053635!2d76.81432579999999!3d8.7522503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05e8216d1d96ad%3A0xefbffee302245ad6!2sKallambalam%20-%20Nagaroor%20Rd%2C%20Kallambalam%2C%20Kerala%20695605!5e0!3m2!1sen!2sin!4v1673078126770!5m2!1sen!2sin" title="myframe" loading="lazy" width="auto" height="250" />
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.3368652611753!2d76.810054!3d8.754341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0!2zOMKwNDUnMTUuNiJOIDc2wrA0OCczNi4yIkU!5e0!3m2!1sen!2sin!4v1674149383994!5m2!1sen!2sin" width="auto" height="250" loading="lazy" ></iframe>
                 </Grid>
             </Page2Container>
         </Page3Wrapper >
